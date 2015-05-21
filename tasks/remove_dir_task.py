369c89fd3c5c08bc2348a3d34120e34739976729
turekj <jkbturek@gmail.com>
2015-05-21 12:10:35 +0200

    Implemented remove directory task

--- tasks/remove_dir_task.py
+++ tasks/remove_dir_task.py
@@ -4,4 +4,12 @@ import os
 
 class RemoveDirectoryTask(core.task.Task):
 	def execute_task(self, parameters=None):
-		pass
+		if isinstance(parameters, dict) and 'path' in parameters:
+			path = parameters['path']
+			
+			if not os.path.exists(path):
+				raise core.task.TaskGenericException('path %s does not exist!' % path)
+			else:
+				os.rmdir(path)
+		else:
+			raise core.task.TaskParameterException('path', 'parameter is mandatory!')
