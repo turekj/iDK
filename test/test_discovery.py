2c2a6978ce0bcd7407372f7d735057a0015ed62e
turekj <jkbturek@gmail.com>
2015-05-21 12:25:13 +0200

    Fixed error in discovery unit test

--- test/test_discovery.py
+++ test/test_discovery.py
@@ -10,9 +10,9 @@ class TestTaskDiscoverer(object):
 	def test_discover_tasks(self):
 		tasks = self.discoverer.discover_tasks()
 		
-		assert len(tasks) == 1
 		assert 'dummy' in tasks
 		assert tasks['dummy'].__class__ is test.res.dummy_task.DummyTask
+		assert 'dummy_object' not in tasks
 
 	def test_topmost_module(self):
 		module = 'some.dummy.module_name'
