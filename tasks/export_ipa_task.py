bc35fa81fce9bc743e246b0a665cab5f4454c145
turekj <jkbturek@gmail.com>
2015-05-22 11:15:15 +0200

    Made export IPA task be able to specify arbitrary provisioning profile during archiving

--- tasks/export_ipa_task.py
+++ tasks/export_ipa_task.py
@@ -1,5 +1,6 @@
 import core.task
 import os
+import re
 
 
 class ExportIPATask(core.task.Task):	
@@ -10,6 +11,21 @@ class ExportIPATask(core.task.Task):
 		archive_path = parameters['archive_path']
 		ipa_path = parameters['ipa_path']
 		provisioning_profile = parameters['provisioning_profile']
+		provisioning_profile_uuid = None
 
-		os.system('xcodebuild -project "%s" -scheme "%s" archive -archivePath "%s"' % (project_path, scheme, archive_path))
+		pattern_str = '\t<key>Name</key>\n\t<string>%s</string>' % provisioning_profile
+		pattern = re.compile(pattern_str)
+
+		for file_name in os.listdir(os.path.expanduser("~/Library/MobileDevice/Provisioning Profiles/")):
+			if file_name.endswith(".mobileprovision"):
+				file_path = '~/Library/MobileDevice/Provisioning Profiles/%s' % file_name
+				with open(os.path.expanduser(file_path), 'r') as file_handle:
+					file_contents = file_handle.read()
+					search_result = pattern.search(file_contents)
+
+					if search_result is not None:
+						provisioning_profile_uuid = file_name[:-16]
+						break
+
+		os.system('xcodebuild -project "%s" -scheme "%s" archive -archivePath "%s" PROVISIONING_PROFILE="%s"' % (project_path, scheme, archive_path, provisioning_profile_uuid))
 		os.system('xcodebuild -exportArchive -exportFormat ipa -archivePath "%s" -exportPath "%s" -exportProvisioningProfile "%s"' % (archive_path, ipa_path, provisioning_profile))
